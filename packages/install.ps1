c9f4905219e77628b6bbbb5f59859f5e2daad5cd
Aditya Thakral <github@9at8.dev>
2022-11-22 16:31:53 -0800

    Update list to include names along with ids
    Fixes #1

--- packages/install.ps1
+++ packages/install.ps1
@@ -1,5 +1,10 @@
 $Packages = Get-Content -Raw -Encoding UTF8 "$PSScriptRoot\list.json" | ConvertFrom-Json
 
 foreach ($package in $Packages) {
-  winget install --id=$package -e
+  $name = $package.name
+  $id = $package.id
+
+  Write-Output "Installing $name ($id) ..."
+
+  winget install --id=$id -e
 }
